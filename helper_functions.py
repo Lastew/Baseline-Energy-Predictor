1aea50362d7682ac5331de912d7b39bc2edc6b94
Sisay <sisfilate@gmail.com>
2019-11-23 14:53:22 -0500

    adding project files

---
+++ helper_functions.py
@@ -0,0 +1,132 @@
+"""
+function for final capstone project.
+"""
+# <h1 style="color:brown;">  Introduction to neural network</h1>
+
+# import data anlysis packages:
+import numpy as np
+from math import sqrt
+
+# import sklearn
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+
+def reduce_mem_usage(df, verbose=True):
+    """Reduces the size of our dataframe."""
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose:
+        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(
+            end_mem, 100 * (start_mem - end_mem)/start_mem))
+    return df
+
+
+def calculate_model_accuracy_metrics(actual, predicted):
+    """
+    Output model accuracy metrics, comparing predicted values
+    to actual values.
+    Arguments:
+        actual: list. Time series of actual values.
+        predicted: list. Time series of predicted values
+    Outputs:
+        Forecast bias metrics, mean absolute error, mean squared error,
+        and root mean squared error in the console
+    """
+    # Calculate forecast bias
+    forecast_errors = [actual[i]-predicted[i] for i in range(len(actual))]
+    bias = sum(forecast_errors) * 1.0/len(actual)
+    print('Bias: %f' % bias)
+    # Calculate mean absolute error
+    mae = mean_absolute_error(actual, predicted)
+    print('MAE: %f' % mae)
+    # Calculate mean squared error and root mean squared error
+    mse = mean_squared_error(actual, predicted)
+    print('MSE: %f' % mse)
+    rmse = sqrt(mse)
+    print('RMSE: %f' % rmse)
+
+
+def split_timestamp(df):
+    """Split time_series."""
+    df['month'] = df['timestamp'].dt.month.astype('uint8')
+    df['day_of_month'] = df['timestamp'].dt.day.astype('uint8')
+    df['day_of_week'] = df['timestamp'].dt.dayofweek.astype('uint8')
+    df['hour'] = df['timestamp'].dt.hour.astype('uint8')
+
+    return df
+
+
+def order_difference(data, col='chilledwater'):
+    """Create dataset with order difference."""
+    data_diff = data.copy()
+    data_diff[col] = data[col].diff()
+    data_diff.dropna(inplace=True)
+    return data_diff
+
+
+def feat_value_count(df, colname):
+    """Value count of each feature
+
+    Args
+    df: data frame.
+    colname: string. Name of to be valued column
+
+    Returns
+    df_count: data frame.
+    """
+    df_count = df[colname].value_counts().to_frame().reset_index()
+    df_count = df_count.rename(columns={'index': colname+'_values', colname: 'counts'})
+    return df_count
+
+
+def feat_value_count(df, colname):
+    """Count the value of each
+    Args
+    df: data frame.
+    colname: string. Name of to be valued column
+
+    Returns
+    df_count: data frame.
+    """
+    df_count = df[colname].value_counts().to_frame().reset_index()
+    df_count = df_count.rename(columns={'index': colname+'_values', colname: 'counts'})
+    return df_count
+
+
+def check_missing(df, cols=None, axis=0):
+    """Check for missing values
+    Args
+    df: data frame.
+    cols: list. List of column names
+    axis: int. 0 means column and 1 means row
+
+    Returns
+    missing_info: data frame.
+    """
+    if cols != None:
+        df = df[cols]
+    missing_num = df.isnull().sum(axis).to_frame().rename(columns={0: 'missing_num'})
+    missing_num['missing_percent'] = df.isnull().mean(axis)*100
+    return missing_num.sort_values(by='missing_percent', ascending=False)
