c07f18a629934896faa1da0a1f41e666f3981587
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-26 01:06:34 +0000

    git push

--- helper_functions.py
+++ helper_functions.py
@@ -130,3 +130,64 @@ def check_missing(df, cols=None, axis=0):
     missing_num = df.isnull().sum(axis).to_frame().rename(columns={0: 'missing_num'})
     missing_num['missing_percent'] = df.isnull().mean(axis)*100
     return missing_num.sort_values(by='missing_percent', ascending=False)
+
+
+def plot_comparison(start_idx, model=model,length=100, train=True, x=x_train_scaled, x=x_test_scaled, y_true=y_train, y_true=y_test):
+    """
+    Plot the predicted and true output-signals.
+    
+    :param start_idx: Start-index for the time-series.
+    :param length: Sequence-length to process and plot.
+    :param train: Boolean whether to use training- or test-set.
+    """
+    
+    if train:
+        # Use training-data.
+        x = x_train_scaled
+        y_true = y_train
+    else:
+        # Use test-data.
+        x = x_test_scaled
+        y_true = y_test
+    
+    # End-index for the sequences.
+    end_idx = start_idx + length
+    
+    # Select the sequences from the given start-index and
+    # of the given length.
+    x = x[start_idx:end_idx]
+    y_true = y_true[start_idx:end_idx]
+    
+    # Input-signals for the model.
+    x = np.expand_dims(x, axis=0)
+
+    # Use the model to predict the output-signals.
+    y_pred = model.predict(x)
+    
+    # The output of the model is between 0 and 1.
+    # Do an inverse map to get it back to the scale
+    # of the original data-set.
+    y_pred_rescaled = y_scaler.inverse_transform(y_pred[0])
+    
+    # For each output-signal.
+    for signal in range(len(target_names)):
+        # Get the output-signal predicted by the model.
+        signal_pred = y_pred_rescaled[:, signal]
+        
+        # Get the true output-signal from the data-set.
+        signal_true = y_true[:, signal]
+
+        # Make the plotting-canvas bigger.
+        plt.figure(figsize=(15,5))
+        
+        # Plot and compare the two signals.
+        plt.plot(signal_true, label='true')
+        plt.plot(signal_pred, label='pred')
+        
+        # Plot grey box for warmup-period.
+        p = plt.axvspan(0, warmup_steps, facecolor='black', alpha=0.15)
+        
+        # Plot labels etc.
+        plt.ylabel(target_names[signal])
+        plt.legend()
+        plt.show()
